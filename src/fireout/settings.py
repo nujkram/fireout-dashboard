4f3445577557021e8b8fbbac668d86fc1175fbfc
Kleo Bercero <kbeflo@gmail.com>
2021-11-23 08:12:47 +0800

    feat: setup dev settings

--- src/fireout/settings.py
+++ src/fireout/settings.py
@@ -1,12 +1,9 @@
 from os import path
 from split_settings.tools import include
 
-if path.isfile('/home/vagrant/fireout-dashboard/envars/local-mark'):
-    print('connecting to local-mark settings...')
-    include('settings_collection/local-mark.py')
-elif path.isfile('/app/test-server.env'):
-    print('connecting to test-server settings...')
-    include('settings_collection/test-server.py')
-else:
+if path.isfile('/app/dev.env'):
+    print('connecting to development settings...')
+    include('settings_collection/dev.py')
+elif path.isfile('/app/prod.env'):
     print('connecting to production settings...')
     include('settings_collection/production.py')
